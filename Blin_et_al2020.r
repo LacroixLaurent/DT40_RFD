df99d2255f0591dff5c98b7fdf0879da859e7121
LacroixLaurent <laurent.lacroix@inserm.fr>
2020-10-26 15:47:02 +0100

    new version

--- Blin_et_al2020.r
+++ Blin_et_al2020.r
@@ -1,5 +1,5 @@
 #### Data treatment for Blin et al 2020
-### LL 20200917
+### LL 20201026
 ### laurent.lacroix@inserm.fr
 
 library("BSgenome.Ggallus.UCSC.galGal4")
@@ -19,9 +19,10 @@ DMD <- GRanges(seqnames="chr1",ranges=IRanges(start=113880001,end=114960000), st
 CCSER1 <- GRanges(seqnames="chr4",ranges=IRanges(start=34950001,end=35660000), strand="*",seqinfo=seqinf)
 
 ## Import OKseq reads
-gra_dmd <- import("data/DMD_OKreads_exp.bed")
+gra_dmd <- import("data/galGal4_DMD_OKreads_exp.bed")
+seqlevels(gra_dmd,pruning.mode="coarse") <- seqlevels(seqinf)
 seqinfo(gra_dmd) <- seqinf
-gra_ccser1 <- import("data/CCSER1_OKreads_exp.bed")
+gra_ccser1 <- import("data/galGal4_CCSER1_OKreads_exp.bed")
 seqlevels(gra_ccser1,pruning.mode="coarse") <- seqlevels(seqinf)
 seqinfo(gra_ccser1) <- seqinf
 
@@ -32,29 +33,38 @@ gr_ccser1 <- gra_ccser1[gra_ccser1 %within% CCSER1]
 
 # compute RFD for OKseq and SM  without binning and with 10 kb bins
 
-sm.wtdmd.nt <- XLSX_track_import(xlfile="data/Track_coordinates.xlsx",feuille=3,outname='wtDMD',bs=10000,na2zero=F,bin=F,expor=T,saverdata=F)
+smwtdmd <- newXLSX_track_import(xlfile="data/Coordonnees_polarite.xlsx",feuille=3)
+seqlevels(smwtdmd, pruning.mode="coarse") <- seqlevels(seqinf)
+seqinfo(smwtdmd) <- seqinf
+smwtdmd.g4 <- smwtdmd[smwtdmd %within% DMD]
 
-sm.wtdmd.10k <- XLSX_track_import(xlfile="data/Track_coordinates.xlsx",feuille=3,outname='wtDMD',bs=10000,na2zero=F,bin=T,expor=T,saverdata=F)
+sm.wtdmd.nt <- makeRFD(smwtdmd.g4,lr=5,na2zero=F,bin=F,datatype="OKseq",export=T,saveRData=F,retur=T,outname="sm_OK_wtDMD_galGal4",OKcheck=F)
+sm.wtdmd.10k <- makeRFD(smwtdmd.g4,lr=5,na2zero=F,bin=T,bs=10000,datatype="OKseq",export=T,saveRData=F,retur=T,outname="sm_OK_wtDMD_galGal4",OKcheck=F)
 
-sm.tetdmd.nt <- XLSX_track_import(xlfile="data/Track_coordinates.xlsx",feuille=4,outname='tetDMD',bs=10000,na2zero=F,bin=F,expor=T,saverdata=F)
 
-sm.tetdmd.10k <- XLSX_track_import(xlfile="data/Track_coordinates.xlsx",feuille=4,outname='tetDMD',bs=10000,na2zero=F,bin=T,expor=T,saverdata=F)
+smtetdmd <- newXLSX_track_import(xlfile="data/Coordonnees_polarite.xlsx",feuille=4)
+seqlevels(smtetdmd, pruning.mode="coarse") <- seqlevels(seqinf)
+seqinfo(smtetdmd) <- seqinf
+smtetdmd.g4 <- smtetdmd[smtetdmd %within% DMD]
 
+sm.tetdmd.nt <- makeRFD(smtetdmd.g4,lr=5,na2zero=F,bin=F,datatype="OKseq",export=T,saveRData=F,retur=T,outname="sm_OK_tetDMD_galGal4",OKcheck=F)
+sm.tetdmd.10k <- makeRFD(smtetdmd.g4,lr=5,na2zero=F,bin=T,bs=10000,datatype="OKseq",export=T,saveRData=F,retur=T,outname="sm_OK_tetDMD_galGal4",OKcheck=F)
 
 # and OKseq
 ## larger DMD ROI
-res_10k_dmdlarge <- makeRFD(gra_dmd,bs=10000,lr=0,na2zero=F,bin=T,datatype='OKseq',export=T,saveRData=F,retur=T,outname='DT40_OK_DMDlarge',OKcheck=F)
+res_10k_dmdlarge <- makeRFD(gra_dmd,bs=10000,lr=0,na2zero=F,bin=T,datatype="OKseq",export=T,saveRData=F,retur=T,outname="DT40_OK_DMDlarge_galGal4",OKcheck=F)
 ## and for just DMD
-res_10k_dmd <- makeRFD(gr_dmd,bs=10000,lr=0,na2zero=F,bin=T,datatype='OKseq',export=T,saveRData=F,retur=T,outname='DT40_OK_DMD',OKcheck=F)
+res_10k_dmd <- makeRFD(gr_dmd,bs=10000,lr=0,na2zero=F,bin=T,datatype="OKseq",export=T,saveRData=F,retur=T,outname="DT40_OK_DMD_galGal4",OKcheck=F)
+
 ### compute correlation
 cor.rfd.test2(res_10k_dmd[[4]], sm.wtdmd.10k[[4]],bs0=10000,binned=T)
 #	Spearman's rank correlation rho
 #data:  as.numeric(unlist(a)[!is.na(unlist(a)) & !is.na(unlist(b))]) and as.numeric(unlist(b)[!is.na(unlist(a)) & !is.na(unlist(b))])
-#S = 39127, p-value < 2.2e-16
+#S = 35279, p-value < 2.2e-16
 #alternative hypothesis: true rho is not equal to 0
 #sample estimates:
-#      rho
-#0.8136212
+#     rho
+#0.831951
 
 
 ### try to evaluate the subsampling required to get the same coverage as the combing RFD
@@ -67,51 +77,52 @@ summary(sm.dmd.cv.10k)
 
 ### 100 sampling taking 2500 reads mapping on the ROI
 n=2500
-bs=10000
+bs0=10000
 set.seed(42)
 res_list10k <- mclapply(1:100, function(i)
 {gr_dmd_sub <- sample(gr_dmd,n)
-resall_dmd_sub <- makeRFD(gr_dmd_sub,bs=10000,lr=0,na2zero=F,bin=T,datatype='OKseq',export=F,saveRData=F,retur=T,OKcheck=F)},mc.cores=4L)
-save(res_list10k,file="res_list10k_DMD.RData")
+resall_dmd_sub <- makeRFD(gr_dmd_sub,bs=bs0,lr=0,na2zero=F,bin=T,datatype="OKseq",export=F,saveRData=F,retur=T,OKcheck=F)},mc.cores=4L)
+save(res_list10k,file="res_list10k_DMD_galGal4.RData")
 
 dmd.rfds10k <- lapply(res_list10k, function(x) {
 	y <- x[[4]][DMD][[1]]
 	z <- y
-	runLength(z) <- runLength(y)/bs
+	runLength(z) <- runLength(y)/bs0
 	return(z)
 })
 
 dmd.rfds10k.cov <- lapply(res_list10k, function(x) {
 	y <- x[[1]][DMD][[1]]
 	z <- y
-	runLength(z) <- runLength(y)/bs
+	runLength(z) <- runLength(y)/bs0
 	return(z)
 })
 smdata <- sm.wtdmd.10k[[4]][DMD][[1]]
 smdata2 <- smdata
-runLength(smdata2) <- runLength(smdata)/bs
+runLength(smdata2) <- runLength(smdata)/bs0
 
 smdata.cov <- sm.wtdmd.10k[[1]][DMD][[1]]
 smdata2.cov <- smdata.cov
-runLength(smdata2.cov) <- runLength(smdata.cov)/bs
+runLength(smdata2.cov) <- runLength(smdata.cov)/bs0
 
-xmin <- 11388
-xmax <-11495
+xmin <- 113880000/bs0
+xmax <-114960000/bs0-1
 
-pdf(file="subsample_DMD_10k.pdf",height=4,width=6)
-plot(smdata2,x=(xmin:xmax),col="red",type="l",lwd=2,ylim=c(-1,1),ylab="RFD",xlab="coordinates (chr1, *10kb)")
+pdf(file="subsample_DMD_10k_galGal4.pdf",height=4,width=6)
+plot(smdata2,x=(xmin:xmax),col="red",type="l",lwd=2,ylim=c(-1,1),ylab="RFD",xlab=paste0("coordinates (chr1, *",bs0/1000,"kb)"))
 dummy <- lapply(dmd.rfds10k, function(y) lines(y,x=(xmin:xmax),col=rgb(0,0,1,0.2)))
 lines(smdata2,x=(xmin:xmax),col="red",type="l",lwd=2)
-legend('topright',legend=c('subsampled(n=2500)','single molecule RFD'), text.col=c('blue','red'),bty='n')
+abline(h=0)
+legend("topright",legend=c(paste0("subsampled(n=",n,")"),"single molecule RFD"), text.col=c("blue","red"),bty="n")
 
-plot(smdata2.cov,x=(xmin:xmax),col="red",type="l",lwd=2,ylim=c(0,50),ylab="Coverage",xlab="coordinates (chr1, *10kb)")
+plot(smdata2.cov,x=(xmin:xmax),col="red",type="l",lwd=2,ylim=c(0,50),ylab="Coverage",xlab=paste0("coordinates (chr1, *",bs0/1000,"kb)"))
 dummy <- lapply(dmd.rfds10k.cov, function(y) lines(y,x=(xmin:xmax),col=rgb(0,0,1,0.2)))
 lines(smdata2.cov,x=(xmin:xmax),col="red",type="l",lwd=2)
-legend('topright',legend=c('subsampled(n=2500)','single molecule'), text.col=c('blue','red'),bty='n')
+legend("topright",legend=c(paste0("subsampled(n=",n,")"),"single molecule"), text.col=c("blue","red"),bty="n")
 dev.off()
 
-### REM and OEM
-## REM
+
+## REM (I-T profiles)
 # WT DMD
 ori_dmd <- import("data/Ini_wtDMD.bed")
 ter_dmd <- import("data/Ter_wtDMD.bed")
@@ -129,7 +140,7 @@ dmd10k$ni <- dmd10ki$ni*bs
 dmd10k$nt <- dmd10kt$nt*bs
 dmd10k$rem <- dmd10k$ni-dmd10k$nt
 cvdmd10krem_wt <- coverage(dmd10k,weight=dmd10k$rem)
-export(cvdmd10krem_wt,con="REM_WTDMD_10k.bw")
+export(cvdmd10krem_wt,con="REM_WTDMD_10k_galGal4.bw")
 
 # TET DMD
 ori_dmd <- import("data/Ini_tetDMD.bed")
@@ -148,7 +159,7 @@ dmd10k$ni <- dmd10ki$ni*bs
 dmd10k$nt <- dmd10kt$nt*bs
 dmd10k$rem <- dmd10k$ni-dmd10k$nt
 cvdmd10krem_tet <- coverage(dmd10k,weight=dmd10k$rem)
-export(cvdmd10krem_tet,con="REM_TETDMD_10k.bw")
+export(cvdmd10krem_tet,con="REM_TETDMD_10k_galGal4.bw")
 
 ## OEM
 ## at 10k on 10k RFD
@@ -161,13 +172,12 @@ cvLn <- cvL/cvT
 oem0 <- endoapply(cvLn, function(cv) c(Rle(rep(NA,(win))),cv)-c(cv,Rle(rep(NA,(win)))))
 oem1 <- endoapply(oem0, function(x) x[(1+win/2):(length(x)-(win/2))])
 oem10k_wtDMD10k <- oem1
-export(oem10k_wtDMD10k,con="OEM_WTDMD_10k.bw")
+export(oem10k_wtDMD10k,con="OEM_WTDMD_10k_galGal4.bw")
 
 oem.t <- oem10k_wtDMD10k[DMD-win][[1]]
 rem.t <- cvdmd10krem_wt[DMD-win][[1]]
 cor.rfd.test3(oem.t,rem.t,binned=T,bs0=5000)
 #	Spearman's rank correlation rho
-#
 #data:  as.numeric(unlist(a)[!is.na(unlist(a)) & !is.na(unlist(b))]) and as.numeric(unlist(b)[!is.na(unlist(a)) & !is.na(unlist(b))])
 #S = 906425, p-value = 6.527e-11
 #alternative hypothesis: true rho is not equal to 0
@@ -182,13 +192,12 @@ cvLn <- cvL/cvT
 oem0 <- endoapply(cvLn, function(cv) c(Rle(rep(NA,(win))),cv)-c(cv,Rle(rep(NA,(win)))))
 oem1 <- endoapply(oem0, function(x) x[(1+win/2):(length(x)-(win/2))])
 oem10k_tetDMD10k <- oem1
-export(oem10k_tetDMD10k,con="OEM_TETDMD_10k.bw")
+export(oem10k_tetDMD10k,con="OEM_TETDMD_10k_galGal4.bw")
 
 oem.t <- oem10k_tetDMD10k[DMD-win][[1]]
 rem.t <- cvdmd10krem_tet[DMD-win][[1]]
 cor.rfd.test3(oem.t,rem.t,binned=T,bs0=5000)
 #	Spearman's rank correlation rho
-#
 #data:  as.numeric(unlist(a)[!is.na(unlist(a)) & !is.na(unlist(b))]) and as.numeric(unlist(b)[!is.na(unlist(a)) & !is.na(unlist(b))])
 #S = 664755, p-value < 2.2e-16
 #alternative hypothesis: true rho is not equal to 0
@@ -201,78 +210,85 @@ cor.rfd.test3(oem.t,rem.t,binned=T,bs0=5000)
 
 ### compute RFD for SM  without binning and with 10 kb bins
 
-sm.wtccser1.nt <- XLSX_track_import(xlfile="data/Track_coordinates.xlsx",feuille=1,outname='wtCCSER1',bs=10000,na2zero=T,bin=F,expor=T,saverdata=F)
+smwtccser1 <- newXLSX_track_import(xlfile="data/Coordonnees_polarite.xlsx",feuille=1)
+seqlevels(smwtccser1, pruning.mode="coarse") <- seqlevels(seqinf)
+seqinfo(smwtccser1) <- seqinf
+smwtccser1.g4 <- smwtccser1[smwtccser1 %within% CCSER1]
 
-sm.wtccser1.10k <- XLSX_track_import(xlfile="data/Track_coordinates.xlsx",feuille=1,outname='wtCCSER1',bs=10000,na2zero=T,bin=T,expor=T,saverdata=F)
+sm.wtccser1.nt <- makeRFD(smwtccser1.g4,lr=5,na2zero=F,bin=F,datatype="OKseq",export=T,saveRData=F,retur=T,outname="sm_OK_wtCCSER1_galGal4",OKcheck=F)
+sm.wtccser1.10k <- makeRFD(smwtccser1.g4,lr=5,na2zero=F,bin=T,bs=10000,datatype="OKseq",export=T,saveRData=F,retur=T,outname="sm_OK_wtCCSER1_galGal4",OKcheck=F)
 
-sm.betaccser1.nt <- XLSX_track_import(xlfile="data/Track_coordinates.xlsx",feuille=2,outname='betaCCSER1',bs=10000,na2zero=T,bin=F,expor=T,saverdata=F)
+smbetaccser1 <- newXLSX_track_import(xlfile="data/Coordonnees_polarite.xlsx",feuille=2)
+seqlevels(smbetaccser1, pruning.mode="coarse") <- seqlevels(seqinf)
+seqinfo(smbetaccser1) <- seqinf
+smbetaccser1.g4 <- smbetaccser1[smbetaccser1 %within% CCSER1]
 
-sm.betaccser1.10k <- XLSX_track_import(xlfile="data/Track_coordinates.xlsx",feuille=2,outname='betaCCSER1',bs=10000,na2zero=T,bin=T,expor=T,saverdata=F)
+sm.betaccser1.nt <- makeRFD(smbetaccser1.g4,lr=5,na2zero=F,bin=F,datatype="OKseq",export=T,saveRData=F,retur=T,outname="sm_OK_betaCCSER1_galGal4",OKcheck=F)
+sm.betaccser1.10k <- makeRFD(smbetaccser1.g4,lr=5,na2zero=F,bin=T,bs=10000,datatype="OKseq",export=T,saveRData=F,retur=T,outname="sm_OK_betaCCSER1_galGal4",OKcheck=F)
 
 # and OKseq with 10kb bins
 ## larger CCSER1 ROI
-res_10k_ccser1large <- makeRFD(gra_ccser1,bs=10000,lr=0,na2zero=F,bin=T,datatype='OKseq',export=T,saveRData=F,retur=T,outname='DT40_OK_CCSER1large',OKcheck=F)
+res_10k_ccser1large <- makeRFD(gra_ccser1,bs=10000,lr=0,na2zero=F,bin=T,datatype="OKseq",export=T,saveRData=F,retur=T,outname="DT40_OK_CCSER1large_galGal4",OKcheck=F)
 ## just CCSER1
-res_10k_ccser1 <- makeRFD(gr_ccser1,bs=10000,lr=0,na2zero=F,bin=T,datatype='OKseq',export=T,saveRData=F,retur=T,outname='DT40_OK_CCSER1',OKcheck=F)
+res_10k_ccser1 <- makeRFD(gr_ccser1,bs=10000,lr=0,na2zero=F,bin=T,datatype="OKseq",export=T,saveRData=F,retur=T,outname="DT40_OK_CCSER1_galGal4",OKcheck=F)
 
 ## compute correlation
 
 cor.rfd.test2(res_10k_ccser1[[4]], sm.wtccser1.10k[[4]],bs0=10000,binned=T)
 #	Spearman's rank correlation rho
 #data:  as.numeric(unlist(a)[!is.na(unlist(a)) & !is.na(unlist(b))]) and as.numeric(unlist(b)[!is.na(unlist(a)) & !is.na(unlist(b))])
-#S = 20352, p-value = 4.244e-10
+#S = 20433, p-value = 4.741e-10
 #alternative hypothesis: true rho is not equal to 0
 #sample estimates:
 #      rho
-#0.6587569
+#0.6573985
 
 
 ### Subsampling
 n=2500
-bs=10000
+bs0=10000
 set.seed(42)
 res_list10kc <- mclapply(1:100, function(i)
 {gr_ccser1_sub <- sample(gr_ccser1,n)
-resall_ccser1_sub <- makeRFD(gr_ccser1_sub,bs=10000,lr=0,na2zero=F,bin=T,datatype='OKseq',export=F,saveRData=F,retur=T,OKcheck=F)},mc.cores=4L)
-save(res_list10kc,file="res_list10k_CCSER1.RData")
+resall_ccser1_sub <- makeRFD(gr_ccser1_sub,bs=bs0,lr=0,na2zero=F,bin=T,datatype="OKseq",export=F,saveRData=F,retur=T,OKcheck=F)},mc.cores=4L)
+save(res_list10kc,file="res_list10k_CCSER1_galGal4.RData")
 
 ccsser1.rfds10k <- lapply(res_list10kc, function(x) {
 	y <- x[[4]][CCSER1][[1]]
 	z <- y
-	runLength(z) <- runLength(y)/bs
+	runLength(z) <- runLength(y)/bs0
 	return(z)
 })
 
 ccsser1.rfds10k.cov <- lapply(res_list10kc, function(x) {
 	y <- x[[1]][CCSER1][[1]]
 	z <- y
-	runLength(z) <- runLength(y)/bs
+	runLength(z) <- runLength(y)/bs0
 	return(z)
 })
 smdata <- sm.wtccser1.10k[[4]][CCSER1][[1]]
 smdata2 <- smdata
-runLength(smdata2) <- runLength(smdata)/bs
+runLength(smdata2) <- runLength(smdata)/bs0
 
 smdata.cov <- sm.wtccser1.10k[[1]][CCSER1][[1]]
 smdata2.cov <- smdata.cov
-runLength(smdata2.cov) <- runLength(smdata.cov)/bs
+runLength(smdata2.cov) <- runLength(smdata.cov)/bs0
 
-xmin <- 34950000/bs
-xmax <- 35660000/bs-1
+xmin <- 34950000/bs0
+xmax <- 35660000/bs0-1
 
-pdf(file="subsample_CCSER1_10k.pdf",height=4,width=6)
-plot(smdata2,x=(xmin:xmax),col="red",type="l",lwd=2,ylim=c(-1,1),ylab="RFD",xlab="coordinates (chr4, *10kb)")
+pdf(file="subsample_CCSER1_10k_galGal4.pdf",height=4,width=6)
+plot(smdata2,x=(xmin:xmax),col="red",type="l",lwd=2,ylim=c(-1,1),ylab="RFD",xlab=paste0("coordinates (chr4, *",bs0/1000,"kb)"))
 dummy <- lapply(ccsser1.rfds10k, function(y) lines(y,x=(xmin:xmax),col=rgb(0,0,1,0.2)))
 lines(smdata2,x=(xmin:xmax),col="red",type="l",lwd=2)
 legend('topright',legend=c('subsampled(n=2500)','single molecule RFD'), text.col=c('blue','red'),bty='n')
 
-plot(smdata2.cov,x=(xmin:xmax),col="red",type="l",lwd=2,ylim=c(0,70),ylab="Coverage",xlab="coordinates (chr4, *10kb)")
+plot(smdata2.cov,x=(xmin:xmax),col="red",type="l",lwd=2,ylim=c(0,70),ylab="Coverage",xlab=paste0("coordinates (chr4, *",bs0/1000,"kb)"))
 dummy <- lapply(ccsser1.rfds10k.cov, function(y) lines(y,x=(xmin:xmax),col=rgb(0,0,1,0.2)))
 lines(smdata2.cov,x=(xmin:xmax),col="red",type="l",lwd=2)
 legend('topright',legend=c('subsampled(n=2500)','single molecule'), text.col=c('blue','red'),bty='n')
 dev.off()
 
-### REM and OEM
 ## REM
 ori_ccser1 <- import("data/Ini_wtCCSER1.bed")
 ter_ccser1 <- import("data/Ter_wtCCSER1.bed")
@@ -293,7 +309,7 @@ ccser110k$ni <- ccser110ki$ni*bs
 ccser110k$nt <- ccser110kt$nt*bs
 ccser110k$rem <- ccser110k$ni-ccser110k$nt
 cvccser110krem_wt <- coverage(ccser110k,weight=ccser110k$rem)
-export(cvccser110krem_wt,con="REM_WTCCSER1_10k.bw")
+export(cvccser110krem_wt,con="REM_WTCCSER1_10k_galGal4.bw")
 
 
 ori_ccser1 <- import("data/Ini_betaCCSER1.bed")
@@ -314,7 +330,7 @@ ccser110k$ni <- ccser110ki$ni*bs
 ccser110k$nt <- ccser110kt$nt*bs
 ccser110k$rem <- ccser110k$ni-ccser110k$nt
 cvccser110krem_beta <- coverage(ccser110k,weight=ccser110k$rem)
-export(cvccser110krem_beta,con="REM_BETACCSER1_10k.bw")
+export(cvccser110krem_beta,con="REM_BETACCSER1_10k_galGal4.bw")
 
 ## OEM
 ## at 10k on 10k RFD
@@ -326,7 +342,7 @@ cvLn <- cvL/cvT
 oem0 <- endoapply(cvLn, function(cv) c(Rle(rep(NA,(win))),cv)-c(cv,Rle(rep(NA,(win)))))
 oem1 <- endoapply(oem0, function(x) x[(1+win/2):(length(x)-(win/2))])
 oem10k_wtCCSER1_10k <- oem1
-export(oem10k_wtCCSER1_10k,con="OEM_WTCCSER1_10k.bw")
+export(oem10k_wtCCSER1_10k,con="OEM_WTCCSER1_10k_galGal4.bw")
 
 oem.t <- oem10k_wtCCSER1_10k[CCSER1-win][[1]]
 rem.t <- cvccser110krem_wt[CCSER1-win][[1]]
@@ -346,13 +362,12 @@ cvLn <- cvL/cvT
 oem0 <- endoapply(cvLn, function(cv) c(Rle(rep(NA,(win))),cv)-c(cv,Rle(rep(NA,(win)))))
 oem1 <- endoapply(oem0, function(x) x[(1+win/2):(length(x)-(win/2))])
 oem10k_betaCCSER1_10k <- oem1
-export(oem10k_betaCCSER1_10k,con="OEM_BETACCSER1_10k.bw")
+export(oem10k_betaCCSER1_10k,con="OEM_BETACCSER1_10k_galGal4.bw")
 
 oem.t <- oem10k_betaCCSER1_10k[CCSER1-win][[1]]
 rem.t <- cvccser110krem_beta[CCSER1-win][[1]]
 cor.rfd.test3(oem.t,rem.t,binned=T,bs0=5000)
 #	Spearman's rank correlation rho
-#
 #data:  as.numeric(unlist(a)[!is.na(unlist(a)) & !is.na(unlist(b))]) and as.numeric(unlist(b)[!is.na(unlist(a)) & !is.na(unlist(b))])
 #S = 179042, p-value = 2.27e-14
 #alternative hypothesis: true rho is not equal to 0
@@ -360,3 +375,21 @@ cor.rfd.test3(oem.t,rem.t,binned=T,bs0=5000)
 #      rho
 #0.5912191
 
+### RFD for OK seq data mapped on galGal6
+
+library("BSgenome.Ggallus.UCSC.galGal6")
+genome <- BSgenome.Ggallus.UCSC.galGal6
+seqinf_g6 <- seqinfo(genome)
+seqlevels(seqinf_g6) <- seqlevels(seqinf_g6)[1:35]
+
+DMD_g6 <- GRanges(seqnames="chr1",ranges=IRanges(start=116170001,end=117250000), strand="*",seqinfo=seqinf_g6)
+DMDexp_g6 <- DMD+500000
+
+gra_dmd_g6 <- import("data/galGal6_DMD_OKreads_exp.bed")
+seqlevels(gra_dmd_g6,pruning.mode="coarse") <- seqlevels(seqinf_g6)
+seqinfo(gra_dmd_g6) <- seqinf_g6
+gr_dmd_g6 <- gra_dmd_g6[gra_dmd_g6 %within% DMD_g6]
+
+res_10k_dmd_g6 <- makeRFD(gr_dmd_g6,bs=10000,lr=20,na2zero=F,bin=T,datatype="OKseq",export=T,saveRData=F,retur=T,outname="DT40_OK_DMD_galGal6",OKcheck=F)
+
+res_10k_dmdexp_g6 <- makeRFD(gra_dmd_g6,bs=10000,lr=20,na2zero=F,bin=T,datatype="OKseq",export=T,saveRData=F,retur=T,outname="DT40_OK_DMDexp_galGal6",OKcheck=F)
--- Blin_et_al2020_Function.r
+++ Blin_et_al2020_Function.r
@@ -6,8 +6,8 @@ library(GenomicAlignments)
 
 makeRFD <- function(gr,bs=1000,lr=1,na2zero=F,bin=T,datatype='OKseq',export=F,saveRData=F,retur=T,outname='myRFDdata',OKcheck=T)
 {
-### Import data from GenomicRanges with the convention that strand=- for rightward forks and strand=+ for leftward forks
-### Output total, R and L coverage as well as RFD. It also filters out low reads regions from the RFD and export the coordinates of those regions.
+### Import data from GenomicRanges with the convention that strand=- for right progressing forks and strand=+ for left progressing forks
+### Output total, R anf L coverage as well as RFD. It also filters out low reads regions from the RFD and export the coordinates of those regions.
 ### Output also BigWig files for the 3 coverages and the low reads regions filtered RFD
 require(GenomicRanges)
 require(rtracklayer)
@@ -22,7 +22,7 @@ mi <- length(gr[strand(gr)=='-'])
 # strand inversion if not classical OKseq data
 if (datatype=='nanopore')
 	{
-# invert 'strand' as in the datafile, -=leftward and +=rightward forks
+# invert 'strand' as in the datafile, -=left progressing and +=right progressing
 	stra <- strand(gr)
 	str2 <- stra
 	str2[stra=='+'] <- '-'
@@ -36,8 +36,8 @@ if (bin)
 
 	bingenp <- countOverlaps(bingen,gr[strand(gr)=='+'])
 	bingenm <- countOverlaps(bingen,gr[strand(gr)=='-'])
-	cv_L <- coverage(bingen,weight=bingenp)	# leftward fork, plus in Wu et al 2018 and W in Petryk et al 2016
-	cv_R <- coverage(bingen,weight=bingenm)	# rightward fork, minus in Wu et al 2018 and C in Petryk et al 2016
+	cv_L <- coverage(bingen,weight=bingenp)	# left progressing fork, plus for Xia's data, W in OKseq paper
+	cv_R <- coverage(bingen,weight=bingenm)	# right progressing fork, minus for Xia's data, C in OKseq paper
 	}
 else
 	{
@@ -45,7 +45,7 @@ else
 	cv_R <- coverage(gr[strand(gr)=='-'])
 	bs=1
 	}
-# in a whole genome OKseq experiment, plus and minus strand should be balanced, if not a correction is implemented
+# in a whole genome OKseq experiment, plus and minus strand shold be balanced, if not a correction is implemented
 if (OKcheck)
 {
 if (abs(corpm-1)>0.01) {print('data are imbalanced, correction required');print(corpm); cv_R <- cv_R*corpm}
@@ -67,7 +67,7 @@ if (na2zero)
 	RFD2[is.na(RFD2)] <- 0
 	naname <- '_noNA'
 	}
-# Option to export results into BigWig files
+# Option to export resuls into BigWig files
 if (export)
 	{
 		rtracklayer::export(cv,con=paste0(outname,'_cov_tot_bs',bs/1000,'k_lr',lr,'.bw'))
@@ -82,67 +82,26 @@ if (saveRData)
 {
 save(res,file=paste0(outname,'_bs',bs/1000,'k_lr',lr,naname,'.RData'))
 }
-# Option to output the result of the function in the current R session
+# Option to output the result of the funtion in the current R session
 if (retur) {return(res)}
 }
 
-
-XLSX_track_import <- function(xlfile="Coordonnees_polarite.xlsx",feuille=1,outname="wtCCSER1",bs=1000,na2zero=T,bin=T,expor=T,saverdata=T)
+newXLSX_track_import <- function(xlfile="Coordonnees_polarite.xlsx",feuille=1)
 {
-# function used to import tracks coordinate from the XLSX file and generate RFD and coverages as in the makeRFD function
-require("rtracklayer")
+# function used to import tracks coordinate from the XLSX file
 require("openxlsx")
 require("GenomicRanges")
-require("BSgenome.Ggallus.UCSC.galGal4")
-genome <- BSgenome.Ggallus.UCSC.galGal4
-seqinf <- seqinfo(genome)
 df1 <- read.xlsx(xlfile,sheet=feuille)
 # set strand/fork orientation as for OKseq reads
 df1$strand <- '*'
 df1$strand[df1$Orientation=='G'] <- '+'
 df1$strand[df1$Orientation=='D'] <- '-'
 
-df1.GR <- sort(GRanges(seqnames=df1$Chromosome,ranges=IRanges(df1$start,df1$end),strand=df1$strand,seqinfo=seqinf))
-# optional binning
-if (bin)
-	{
-	bingen <- do.call(c,lapply( (1:length(seqlevels(df1.GR))),function(y) a=GRanges(seqnames=seqnames(seqinfo(df1.GR))[y],ranges=breakInChunks(totalsize=seqlengths(seqinfo(df1.GR))[y],chunksize=bs),strand='*',seqinfo=seqinfo(df1.GR))))
-	bingenp <- countOverlaps(bingen,df1.GR[strand(df1.GR)=='+'])
-	bingenm <- countOverlaps(bingen,df1.GR[strand(df1.GR)=='-'])
-	df1.cvL <- coverage(bingen,weight=bingenp)	# leftward fork
-	df1.cvR <- coverage(bingen,weight=bingenm)	# rightward fork
-	}
-else
-	{
-	df1.cvL <- coverage(df1.GR[strand(df1.GR)=='+'])
-	df1.cvR <- coverage(df1.GR[strand(df1.GR)=='-'])
-	bs=1
-	}
-# compute total coverage and RFD
-df1.cv <- df1.cvL+df1.cvR
-df1.RFD <- (df1.cvR-df1.cvL)/(df1.cvL+df1.cvR)
-# Option to set out NA to 0 in RFD for easier import of the files in IGV but beware that it could change correlation
-if (na2zero)
-	{
-	df1.RFD[is.na(df1.RFD)] <- 0
-	}
-
-res <- list(df1.cv,df1.cvL,df1.cvR,df1.RFD,df1.GR)
-names(res) <- c("cv","cvL","cvR","RFD","forks")
-# Option to export results into BigWig files
-if (expor==T)
-{
-export(df1.RFD,con=paste0(outname,'_RFD_bs',bs/1000,'k.bw'))
-export(df1.cv,con=paste0(outname,'_cov_tot_bs',bs/1000,'k.bw'))
-export(df1.cvL,con=paste0(outname,'_cov_2left_bs',bs/1000,'k.bw'))
-export(df1.cvR,con=paste0(outname,'_cov_2right_bs',bs/1000,'k.bw'))
-}
-# Option to save the results in a RData file
-if (saverdata==T) {save(res,file=paste0(outname,".RData"))}
-return(res)
+df1.GR <- sort(GRanges(seqnames=df1$Chromosome,ranges=IRanges(df1$start,df1$end),strand=df1$strand))
+return(df1.GR)
 }
 
-### a function to check correlation genome-wide between RFD disregarding places with NA
+### a function to check correlation genome-wide between RFD disregarding places with NA 
 ### for binned values
 cor.rfd.test2 <- function(a,b,met='s',binned=T,bs0=bs,...)
 {
